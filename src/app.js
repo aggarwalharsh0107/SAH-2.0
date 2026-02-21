c7faf0b388ccb5746f6681cbc0b0c8993c8a9090
Sparrowsyntax3210 <Sparrowsyntax3210@gmail.com>
2026-02-21 16:37:03 +0530

    Login Form

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
-const express = require("express");
-const multer = require("multer");
-const path = require("path");
-
+const express = require('express');
 const app = express();
+const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
-// View engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => res.send("Server running"));
-
-app.post("/upload", upload.array("files"), (req, res) => {
-    setTimeout(() => {
-        res.json({ success: true });
-    }, 1500);
-});
 
 module.exports = app;
 
-app.listen(4000, () => console.log("Server running on http://localhost:4000"));
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
+app.post("/upload", upload.single("files"), (req, res) => {
+    res.json({ success: true });
+});
